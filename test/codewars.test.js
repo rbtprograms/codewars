fcaf789059e32143111bb8ab8b9a6cba89f416ec
rbtprograms <rbt61690@gmail.com>
2018-11-05 07:02:04 -0800

    dubstep eval done

--- test/codewars.test.js
+++ test/codewars.test.js
@@ -5,10 +5,20 @@ const {
   smallEnough,
   findOutlier,
   dutyFree,
-  listPosition } = require('../lib/codewars');
+  listPosition,
+  songDecoder } = require('../lib/codewars');
 
 describe('Codewars testing', () => {
   let tester;
+
+  describe.only('song decoder tests', () => {
+    it('should pass the tests', () => {
+      assert.equal(songDecoder('AWUBBWUBC'), 'A B C', 'WUB should be replaced by 1 space');
+      assert.equal(songDecoder('AWUBWUBWUBBWUBWUBWUBC'), 'A B C', 'multiples WUB should be replaced by only 1 space');
+      assert.equal(songDecoder('WUBAWUBBWUBCWUB'), 'A B C', 'heading or trailing spaces should be removed');
+
+    });
+  });
 
   describe('listPosition tests', () => {
     it('should pass the tests', () => {
